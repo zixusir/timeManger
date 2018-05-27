89e51bdf5182f761bb248fc807e8f56af298b3ae
zixusir <zixu_sir@163.com>
2018-05-27 23:11:51 +0800

    完成所有工作，结束这个小项目

--- src/components/user.vue
+++ src/components/user.vue
@@ -15,16 +15,22 @@
           <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
           <mt-field label="密码" placeholder="请输入密码" type="password" v-model="newpsd"></mt-field>
           <mt-field label="确认密码" placeholder="再输入一次" type="password" v-model="secpsd"></mt-field>
-          <mt-button disabled="regable">注册</mt-button>
-          <mt-button>切回登录</mt-button>
+          <mt-button
+            v-bind:disabled="regable"
+            @click="setUser">
+            注册
+          </mt-button>
+          <mt-button @click="tab='login'">切回登录</mt-button>
         </div>
       </div>
       <div v-else-if="tab==='login'">
+        <p v-show="wrongmsg">账户或密码错误</p>
         <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
-        <mt-field label="密码" placeholder="请输入密码" type="password" v-model="newpsd"></mt-field>
+        <mt-field label="密码" placeholder="请输入密码" type="password" v-model="loginpsd"></mt-field>
+        <mt-button @click="setLogin">登录</mt-button>
         <mt-button @click="handleReg">注册新用户</mt-button>
       </div>
-      <div v-else class="page">
+      <div v-else-if="tab==='logined'" class="page">
         <div class="icon">
           <p class="v_center">{{userLogo}}</p>
         </div>
@@ -44,11 +50,12 @@ export default {
       tab: '',
       status: false,
       username: 'John',
-      newuser: '',
-      newemail: '',
+      loginpsd: '',
       newpsd: '',
       secpsd: '',
-      regable: true
+      regable: true,
+      localUser: {},
+      wrongmsg: false
     }
   },
   created: function () {
@@ -56,24 +63,27 @@ export default {
     if (localStorage.hasOwnProperty('list')) {
       this.list = JSON.parse(localStorage.getItem('lists'))
     }
-    if (localStorage.hasOwnProperty('status')) {
-      if (localStorage.hasOwnProperty('status') === 'true') {
-        this.status = true
+    if (localStorage.hasOwnProperty('status') && localStorage.hasOwnProperty('user')) {
+      if (localStorage.getItem('status') === 'true') {
+        this.status = 'true'
+        this.tab = 'logined'
+        this.username = JSON.parse(localStorage.getItem('user')).username
       } else {
-        this.status = false
+        this.status = 'false'
       }
     } else {
-      this.status = false
+      this.status = 'false'
       this.tab = 'register'
       localStorage.setItem('status', this.status)
     }
-    if (localStorage.hasOwnProperty('username')) {
-      this.username = localStorage.getItem('username')
+    if (localStorage.hasOwnProperty('user')) {
+      this.localUser = JSON.parse(localStorage.getItem('user'))
     }
     console.log(this.status)
-    if (!this.status) {
-      console.log('why')
+    if (this.status !== 'true') {
       this.tab = 'login'
+    } else {
+      this.tab = 'logined'
     }
   },
   computed: {
@@ -83,7 +93,7 @@ export default {
   },
   watch: {
     secpsd: 'checksame',
-    newpad: 'checksame'
+    newpsd: 'checksame'
   },
   methods: {
     handleReg: function () {
@@ -91,10 +101,33 @@ export default {
     },
     checksame: function () {
       console.log(this.newpsd + ',' + this.secpsd)
-      if (this.newpsd !== this.secpsd) {
-        // this.regable = false
+      console.log(this.newpsd === this.secpsd)
+      if (this.newpsd === this.secpsd) {
+        this.regable = false
       } else {
-        // this.regable = true
+        this.regable = true
+      }
+    },
+    setUser: function () {
+      let user = {
+        username: this.username,
+        password: this.newpsd
+      }
+      this.localUser = user
+      localStorage.setItem('user', JSON.stringify(user))
+    },
+    setLogin: function () {
+      if (this.localUser.username === this.username && this.localUser.password === this.loginpsd) {
+        this.status = true
+        localStorage.setItem('status', 'true')
+        this.tab = 'logined'
+      } else {
+        this.wrongmsg = true
+        let that = this
+        setTimeout(function () {
+          console.log('密码错误')
+          that.wrongmsg = false
+        }, 1000)
       }
     }
   }
